669d0dbe8e688e9db7ba7a547d92c48b791d2f39
slav.umni <slav.umni@gmail.com>
2024-07-25 03:19:15 +0700

    add docker compose commands

--- docker.mk
+++ docker.mk
@@ -1,2 +1,31 @@
-ps: ## docker ps
-	docker ps
+# --- [ docker compose control ] --------------------------------------------------------------------------------------
+-----------------: ## -----------------
+
+up: ## docker compose up -d (c=<container name>)
+	@docker compose up -d $(c)
+
+up-rec: ## docker compose up -d --force-recreate (c=<container name>)
+	@docker compose up -d --force-recreate $(c)
+
+start: ## docker compose start (c=<container name>)
+	@docker compose start $(c)
+
+stop: ## docker compose stop (c=<container name>)
+	@docker compose start $(c)
+
+restart: ## docker compose restart (c=<container name>)
+	@docker compose restart $(c)
+
+down: ## docker compose down
+	@docker compose down
+
+destroy: ## docker compose destroy
+	@docker compose destroy
+
+# --- [ docker compose commands ] -------------------------------------------------------------------------------------
+-----------------: ## -----------------
+
+logs: ## docker compose logs (c=<container name>)
+	@docker compose logs --timestamps -n 150 $(c)
+ps: ## docker compose ps
+	@docker compose ps
